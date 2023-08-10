40031eea43709d28ecb1fdb448280bcbda876e04
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-10 07:20:39 -0700

    task completed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * discription: C program that prints a fixed messege
+ *
+ * Return: Always zero (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
