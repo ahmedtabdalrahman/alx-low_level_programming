c4595ad2f767821c753916fcca546bd24a083493
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-10 07:56:29 -0700

    task completed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,18 +8,10 @@
  */
 int main(void)
 {
-	/* declaration of variables*/
-	int i;
-	char c;
-	long int li;
-	long long int lli;
-	float f;
-
-	/*printing statement*/
-	printf("size of a char is: %lu.\n byte(s)", (unsigned long)sizeof(c);
-	printf("size of an int is: %lu.\n byte(s)", (unsigned long)sizeof(i);
-	printf("size of a long int is: %lu.\n byte(s)", (unsigned long)sizeof(li);
-	printf("size of a long long int is: %lu.\n byte(s)", (unsigned long)sizeof(lli);
-	printf("size of a float is: %lu.\n byte(s)", (unsigned long)sizeof(f);
+	printf("size of a char is: %lu.\n byte(s)", (unsigned long)sizeof(char);
+	printf("size of an int is: %lu.\n byte(s)", (unsigned long)sizeof(int);
+	printf("size of a long int is: %lu.\n byte(s)", (unsigned long)sizeof(long int);
+	printf("size of a long long int is: %lu.\n byte(s)", (unsigned long)sizeof(long long int);
+	printf("size of a float is: %lu.\n byte(s)", (unsigned long)sizeof(float);
 	return (0);
 }
