3f722c3e3136b7163e9ebe1a61ed459b2859a2f3
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-10 10:27:27 -0700

    advance task completed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,14 @@
 /**
  * main entry point
  *
- * discription: prints to the standard error
+ * description: prints to the standard error
  *
- * Return 1
+ * Return 1 (ERROR)
  */
 int main(void)
 {
-    char c = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, c, 59);
-    return (1);
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, c, 59);
+	return (1);
 }
