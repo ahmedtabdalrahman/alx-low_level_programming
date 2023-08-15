ea3939b584796c846b07a68916b4925733250416
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-15 10:51:34 -0700

    task completed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Discription: Prints the string _putchar
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+int string[] = "_putchar";
+for (int c = 0, c <= 7, c++)
+	_putchar(string[c]);
+_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ *
+ * _putchar Prints the character
+ *
+ * Return
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1);
+}
