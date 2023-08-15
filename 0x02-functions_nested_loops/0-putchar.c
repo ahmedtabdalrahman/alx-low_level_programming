a9ea1dbef062fcc8e58ecc83b04b2c7b62d9e49c
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-15 10:53:45 -0700

    task completed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,8 +10,8 @@
 
 int main(void)
 {
-int string[] = "_putchar";
-for (int c = 0, c <= 7, c++)
+char string[] = "_putchar";
+for (int c = 0; c <= 7; c++)
 	_putchar(string[c]);
 _putchar('\n');
 	return (0);
