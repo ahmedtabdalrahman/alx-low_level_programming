62a00eefc8eefd812d19fe66b226cbcd464e189c
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-17 13:24:32 -0700

    task completed

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,11 +12,9 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for (i = 0; i >= n; i++)
-		{
+		for (i = 0; i <= n; i++)
 			_putchar('-');
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 	else
 		_putchar('\n');
