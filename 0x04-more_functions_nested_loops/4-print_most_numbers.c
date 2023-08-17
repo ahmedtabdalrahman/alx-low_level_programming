daa7958ad33d56d9427d6436b65127a6ead6c6ea
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-17 09:24:56 -0700

    task completed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,8 +9,11 @@ void print_most_numbers(void)
 	int c;
 
 	for (c = 48; c <= 57; c++)
-		if (c != 50 && c != 52)
-			putchar(c);
-	 _putchar('\n');
+	{
+		if (c == 50 || c == 52)
+			continue;
+		putchar(c);
+	}
+	_putchar('\n');
 
 }
