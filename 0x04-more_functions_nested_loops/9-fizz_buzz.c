e814d155689faf0a84ac4a7eedb6b07ee57ef975
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-17 15:11:32 -0700

    task completed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,7 @@
  *
  * Description: The “Fizz-Buzz test”
  *
- * Return - always 0 (success)
+ * Return: always 0 (success)
  */
 
 int main(void)
@@ -14,14 +14,15 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0)
-			printf("FizzBuzz");
-		else if ((i % 3) == 0 && !(i % 5) == 0)
+		if ((i % 3) == 0 && !(i % 5) == 0)
 			printf("Fizz");
 		else if ((i % 5) == 0 && !(i % 3) == 0)
 			printf("Buzz");
+		else if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
 		else
 			printf("%d", i);
+
 		if (i != 100)
 			printf(" ");
 		else
