9cccd3d955b56dc9e36da3b0dab809910700af9b
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-17 13:03:56 -0700

    task completed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,13 @@
 
 void more_numbers(void)
 {
-	int i, j, t;
+	int i;
+	int j;
+	int t;
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; c <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			t = j;
 			if (j > 9)
@@ -18,7 +20,7 @@ void more_numbers(void)
 				_putchar(1 + 48);
 				t = j % 10;
 			}
-			_putchar(j + 48);
+			_putchar(t + 48);
 		}
 		_putchar('\n');
 	}
