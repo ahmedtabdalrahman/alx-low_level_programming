b0d723d23638fd4c69cb5a005e885df464635c54
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-21 14:12:51 -0700

    task completed

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: function input
+ */
+
+void puts2(char *str)
+{
+	int cnt;
+
+	for (cnt = 0; str[cnt] != 0; ++cnt)
+	{
+		if (cnt %2 ==0)
+			_putchar(str[cnt]);
+	}
+	_putchar('\n');
+}
