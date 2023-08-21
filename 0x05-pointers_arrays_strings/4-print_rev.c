fd643a4ec7b29d321ad022df930238956dbe55ca
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-21 13:09:28 -0700

    task completed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ *
+ * @s: function input
+ */
+
+void print_rev(char *s)
+{
+	int cnt;
+
+	while (*s[cnt])
+		i++;
+
+	while (cnt--)
+		_putchar(s[cnt]);
+	_putchar('\n');
+}
