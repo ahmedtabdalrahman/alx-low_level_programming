16ee20568d4a0c16ab5d78927bd18f2ec369efba
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-21 14:50:55 -0700

    task completed

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ *
+ * @str: function input
+ */
+
+void print_array(int *a, int n)
+{
+	int cnt;
+
+	for (cnt = 0; cnt < n; cnt++)
+	{
+		if (cnt != (n - 1))
+			printf("&d, ", a[cnt];
+		else
+			printf("&d", a[cnt];
+	}
+	_putchar(str[j + i];
+}
