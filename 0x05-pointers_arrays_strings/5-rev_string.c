6e21c9cd74d00d3338e29e6f8333a8bc377b7abb
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-21 13:32:22 -0700

    task completed

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ *
+ * @s: function input
+ */
+
+void rev_string(char *s)
+{
+	int cnt, ;
+
+	for (cnt = 0; s[cnt] != 0; cnt++)
+		c = s[i];
+
+	for (i = 0; i <= cnt , i++)
+		d[i] = c[cnt - i];
+		
+	s = d;
+}
