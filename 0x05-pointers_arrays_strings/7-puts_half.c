a3ebd3b264b9bbe57eba3e9ea7fd463af743f6f4
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-21 14:36:50 -0700

    task completed

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ *
+ * @str: function input
+ */
+
+void puts_half(char *str)
+{
+	int cnt, i;
+	char c;
+
+	for (cnt = 0; s[cnt] != 0; ++cnt)
+		;
+
+	i = (cnt + 1) / 2;
+	for (j = 0; j <= i; j++)
+		_putchar(str[j + i];
+}
