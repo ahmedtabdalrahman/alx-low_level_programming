05c38fc85e37aae1a6290654bafb6c3f1b21da50
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-30 11:53:29 -0700

    task completed

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,8 @@
 /**
  * _pow_recursion - prints the value of x raised to the power of y
  *
- * @n: function input
+ * @x: function input
+ * @y: function input
  *
  * Return: the value of x raised to the power of y
  */
@@ -11,10 +12,10 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return(-1);
-	else if (n == 0)
+		return (-1);
+	else if (y == 0)
 		return (1);
-	else if (n == 1)
+	else if (y == 1)
 		return (x);
 	else
 		return (x, _pow_recursion(y - 1));
