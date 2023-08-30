77a1f1a66a8a92eceea7a92169179f4f58200c32
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-08-30 12:15:44 -0700

    task completed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,14 +7,14 @@
  *
  * Return: the natural square root of a number
  */
-int sqrt(int n, int i); 
+int _sqrt(int n, int i); 
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n, 1));
+	return (_sqrt(n, 1));
 }
 
 /**
- * sqrt - prints the natural square root of a number
+ * _sqrt - prints the natural square root of a number
  *
  * @n: function input
  * @i: function input
@@ -22,12 +22,12 @@ int _sqrt_recursion(int n)
  * Return: the natural square root of a number
  */
 
-int sqrt(int n, int i)
+int _sqrt(int n, int i)
 {
 	if (i * i == n)
 		return (i);
 	else if (i * i < n)
-		return (sqrt(n, i + 1));
+		return (_sqrt(n, i + 1));
 	else
 		return (-1);
 }
