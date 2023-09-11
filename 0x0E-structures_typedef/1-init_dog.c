b38ff0bdf949237a39559c569452cedba0abe7dc
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-09-11 14:35:11 -0700

    task done

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <dog.h>
+
+/**
+ * init_dog - function initialization
+ *
+ * @d : function input
+ * @name : function input
+ * @age : function input
+ * @owner : function input
+ *
+ * Return: 0
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
