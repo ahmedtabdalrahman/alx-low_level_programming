a3f6141157e4dc08e88b1bbdf9d8876abe5b572a
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-09-11 13:26:57 -0700

    task done

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_LIKE_MACR0_H
+#define _FUNCTION_LIKE_MACR0_H
+
+#define ABS(X) ((x) < 0 ? (-(x)) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _SUM_H
+#define _SUM_H
+
+#define SUM(X,y) ((x) + (y))
+
+#endif
