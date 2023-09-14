f57773e53fedfe35a7f7029d368faff5d918e4a2
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-09-13 22:04:52 -0700

    deleted for deferment

--- 0x09-static_libraries/README.md
+++
@@ -1,1 +0,0 @@
-this is the README file for "0x09. C - Static libraries"project
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef _FUNCTION_LIKE_MACR0_H
-#define _FUNCTION_LIKE_MACR0_H
-
-#define ABS(x) ((x) < 0 ? (-(x)) : (x))
-
-#endif
