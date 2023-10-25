b879ef31f83bfe971c7656c38ad17d02c5af848a
ahmedtabdalrahman <ahmedtabdalrahman@gmail.com>
2023-10-25 00:32:14 -0700

    task done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that returns the length of a string.
+ *
+ * @s: function input
+ *
+ * Return: the length of a string.
+ */
+
+void _strlen_recursion(char *s)
+{
+	int x = 0;
+
+	if (*s > '\0')
+	
+		x += _strlen_recursion(s + 1) + 1;
+	return (x);
+}
